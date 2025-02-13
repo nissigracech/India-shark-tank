48483419a0a1c7c143595130041f83a9e7c96d9d
nissigracech <nissigracech@gmail.com>
2025-02-13 10:52:43 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -14,3 +14,19 @@ st.markdown("""
     </style>
     <h1 class="title">Shark Tank India EDA Dashboard</h1>
 """, unsafe_allow_html=True)
+
+
+# Create three buttons below the title
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    if st.button("Overview"):
+        st.write("You clicked Overview!")
+
+with col2:
+    if st.button("Investment Analysis"):
+        st.write("You clicked Investment Analysis!")
+
+with col3:
+    if st.button("Startup Insights"):
+        st.write("You clicked Startup Insights!")
