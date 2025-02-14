d8ad720ac2131b581fa25d3b60cc95702a993b3b
nissigracech <nissigracech@gmail.com>
2025-02-14 23:48:09 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -97,7 +97,8 @@ filtered_df['equity_per_shark']=filtered_df['equity_per_shark'].fillna(0)
 
 def classes(argument,season_df):
         st.markdown(f"<h1 style='text-align: center;'>{argument}</h1>", unsafe_allow_html=True)
-        st.write(season_df.info())
+        st.write(season_df.columns)
+        
     
 
 # Set page config
