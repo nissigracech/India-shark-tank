3ac12c66aa8d41ebc8d2dae9baafe21576d04d23
nissigracech <nissigracech@gmail.com>
2025-02-14 16:54:41 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -34,7 +34,7 @@ st.markdown("""
 
         /* Button styling */
         .stButton>button {
-            width: 150px;
+            width: 15px;
             font-weight: bold;
             font-size: 16px;
             padding: 10px;
