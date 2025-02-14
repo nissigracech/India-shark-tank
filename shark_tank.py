ea707e4215e028b3a6958609d85d01c2f6bff68b
nissigracech <nissigracech@gmail.com>
2025-02-14 13:07:22 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -68,7 +68,7 @@ image = Image.open('st.png')
 col1, col2, col3 = st.columns([1, 5, 1])  
 
 with col2:  # Center the content
-    sub_col1, sub_col2, sub_col3 = st.columns([1, 3, 1])  # Nested columns inside col2
+    sub_col1, sub_col2, sub_col3 = st.columns([1, 1, 1])  # Nested columns inside col2
 
     with sub_col2:  # Image inside the middle sub-column
         st.image("stilogo.png", caption="Shark Tank India S1", width=200)
