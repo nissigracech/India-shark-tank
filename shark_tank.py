e0e348c8942366b71ebcdd417d29745920957942
nissigracech <nissigracech@gmail.com>
2025-02-15 20:35:53 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -127,7 +127,15 @@ filtered_df['deal_amount_per_shark']=filtered_df['deal_amount_per_shark'].fillna
 filtered_df['equity_per_shark']=filtered_df['equity_per_shark'].fillna(0)
 
 
-st.markdown(
+
+
+def classes(argument,season_df):
+    st.markdown(f"<h1 style='text-align: center;'>{argument}</h1>", unsafe_allow_html=True)
+    total_pitches = season_df["Pitch Number"].nunique()
+    total_episodes = season_df["Episode Number"].nunique()
+        
+        
+    st.markdown(
     """
     <style>
     .metric-card {
@@ -152,9 +160,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Function to create metric cards
-def metric_card(title, value):
-    st.markdown(
+    # Function to create metric cards
+    def metric_card(title, value):
+        st.markdown(
         f"""
         <div class="metric-card">
             <div class="metric-title">{title}</div>
@@ -162,35 +170,29 @@ def metric_card(title, value):
         </div>
         """,
         unsafe_allow_html=True
-    )
+        )
 
 
-
-def classes(argument,season_df):
-        st.markdown(f"<h1 style='text-align: center;'>{argument}</h1>", unsafe_allow_html=True)
-        total_pitches = season_df["Pitch Number"].nunique()
-        total_episodes = season_df["Episode Number"].nunique()
-        
-        # Creating a layout with columns
-        col1, col2, col3, col4 = st.columns(4)
-        with col1:
-            metric_card("Number of Episodes",  season_df['Episode Number'].nunique())
-        with col2:
-            metric_card("First Aired", "20 Dec 2021")
-        with col3:
-            metric_card("Last Aired", "4 Feb 2022")
-        with col4:
-            metric_card("Host", "Rannvijay Singha")
+    # Creating a layout with columns
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        metric_card("Number of Episodes",  season_df['Episode Number'].nunique())
+    with col2:
+        metric_card("First Aired", "20 Dec 2021")
+    with col3:
+        metric_card("Last Aired", "4 Feb 2022")
+    with col4:
+        metric_card("Host", "Rannvijay Singha")
             
-        col6, col7, col8, col9 = st.columns(4)
-        with col6:
-            metric_card("Number of Pitches", "117")
-        with col7:
-            metric_card("Total Amount of Money Spent", "₹ 41.33 Crores")
-        with col8:
-            metric_card("Invested", "₹ 38.32 Crores")
-        with col9:
-            metric_card("Debt", "₹ 3.01 Crores")
+    col6, col7, col8, col9 = st.columns(4)
+    with col6:
+        metric_card("Number of Pitches", "117")
+    with col7:
+        metric_card("Total Amount of Money Spent", "₹ 41.33 Crores")
+    with col8:
+        metric_card("Invested", "₹ 38.32 Crores")
+    with col9:
+        metric_card("Debt", "₹ 3.01 Crores")
  
 
 # Streamlit UI
