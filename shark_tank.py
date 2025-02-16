415286038c0006c101f7099db9e0e98c837e1f5d
nissigracech <nissigracech@gmail.com>
2025-02-17 01:25:18 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -182,23 +182,23 @@ def classes(argument,season_df):
     # Creating a layout with columns
     col1, col2, col3, col4 = st.columns(4) 
     with col1:
-        metric_card("Number of Episodes",season_df['Episode Number'].nunique())
+        metric_card("Number of Episodes",season_df['Episode Number'].nunique()," ")
     with col2:
-        metric_card("Season start Date", season_df['Season Start'].max())
+        metric_card("Season start Date", season_df['Season Start'].max()," ")
     with col3:
-        metric_card("Season Last date", season_df['Season End'].min())
+        metric_card("Season Last date", season_df['Season End'].min()," ")
     with col4:
-        metric_card("Show Host", season_df['Anchor'].mode()[0])
+        metric_card("Show Host", season_df['Anchor'].mode()[0]," ")
             
     col6, col7, col8, col9 = st.columns(4)
     with col6:
-        metric_card("Total Pitches", season_df['Pitch Number'].nunique())
+        metric_card("Total Pitches", season_df['Pitch Number'].nunique()," ")
     with col7:
-        metric_card("Total Investment(in crores )",f"₹{season_df['Total Deal Amount'].sum() / 100:.2f} crores")
+        metric_card("Total Investment(in crores )",f"₹{season_df['Total Deal Amount'].sum() / 100:.2f} crores"," ")
     with col8:
-        metric_card("Startups That Received Offers", season_df['Received Offer'].eq(1).sum())
+        metric_card("Startups That Received Offers", season_df['Received Offer'].eq(1).sum()," ")
     with col9:
-        metric_card("Startups That Accepted Offers", season_df['Accepted Offer'].eq(1).sum(),"hi")
+        metric_card("Startups That Accepted Offers", season_df['Accepted Offer'].eq(1).sum()," ")
         
 
 
