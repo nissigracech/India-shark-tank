534b5bf58a78928b74904b5d873772e94f0787bd
nissigracech <nissigracech@gmail.com>
2025-02-17 22:18:28 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -190,14 +190,24 @@ def classes(argument,season_df):
     with col4:
         metric_card("Show Host", season_df['Anchor'].mode()[0]," ")
             
-    col6, col7, col8, col9 = st.columns(4)
-    with col6:
+    col5, col6, col7, col8 = st.columns(4)
+    with col5:
         metric_card("Total Pitches", season_df['Pitch Number'].nunique()," ")
-    with col7:
+    with col6:
         metric_card("Total Investment(in crores )",f"₹{season_df['Total Deal Amount'].sum() / 100:.2f} crores"," ")
-    with col8:
+    with col7:
         metric_card("Startups That Received Offers", season_df['Received Offer'].eq(1).sum()," ")
+    with col8:
+        metric_card("Startups That Accepted Offers", season_df['Accepted Offer'].eq(1).sum()," ")
+        
+    col9, col10, col11, col12 = st.columns(4)
     with col9:
+        metric_card("Total Pitches", season_df['Pitch Number'].nunique()," ")
+    with col10:
+        metric_card("Total Investment(in crores )",f"₹{season_df['Total Deal Amount'].sum() / 100:.2f} crores"," ")
+    with col11:
+        metric_card("Startups That Received Offers", season_df['Received Offer'].eq(1).sum()," ")
+    with col12:
         metric_card("Startups That Accepted Offers", season_df['Accepted Offer'].eq(1).sum()," ")
         
 
