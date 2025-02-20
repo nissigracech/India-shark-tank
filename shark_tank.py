a5ace6b8e449d3747920d13ddd0496d0dc55b5d2
nissigracech <nissigracech@gmail.com>
2025-02-21 01:00:20 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -128,7 +128,8 @@ def seasons_data(argument, season_df,season_sharks):
     with col9:
         metric_card("Highest Valuation Given ( in ₹ )", f"₹{season_df['Deal Valuation'].max()/ 100:.2f} crores",season_df.loc[season_df['Deal Valuation'] == season_df['Deal Valuation'].max(), 'Startup Name'].values[0])
     with col10:
-        metric_card("Lowest Valuation Given ( in ₹ )", f" {max(sharks_count,key=sharks_count.get)}",season_df.loc[season_df['Deal Valuation'] == season_df[season_df['Deal Valuation'] > 0]['Deal Valuation'].min(), 'Startup Name'].values[0])
+        max_key = max(data, key=data.get)
+        metric_card("Lowest Valuation Given ( in ₹ )", f" {sharks_count[max_key]}",max_key)
     with col11:
         metric_card("Highest Equity Given ( in ₹ )", f"₹{season_df['Total Deal Equity'].max()} %",season_df.loc[season_df['Total Deal Equity'] == season_df['Total Deal Equity'].max(), 'Startup Name'].values[0])
     with col12:
