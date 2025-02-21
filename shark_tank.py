7cb0981b6f6847316b10ce80ba6bf7e231b14993
nissigracech <nissigracech@gmail.com>
2025-02-21 22:51:41 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -118,9 +118,43 @@ def count_shark_deal(data, shark_names):
              deal_counts[shark_name]=data[deal_column].sum() 
     return deal_counts
 
+ 
+def shark_info_card(shark_name, occupation, education, image_filename):
+    """Creates a styled card for shark information."""
+
+     
+
+    st.markdown(f"""
+    <div style="border: 1px solid #ccc; padding: 20px; border-radius: 10px; margin-bottom: 20px;">
+        <div style="display: flex; align-items: center;">  
+            <div style="width: 150px; margin-right: 20px;">  
+                <img src="{image_filename}" style="width: 100%; border-radius: 5px;">
+            </div>
+            <div>
+                <h3 style="margin-top: 0;">{shark_name}</h3>
+                <p><strong>Occupation:</strong> {occupation}</p>
+                <p><strong>Education:</strong> {education}</p>
+            </div>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
 
 def sharks_info(season_df,key):
     image_info=key+".jpg"
+    st.markdown(f"""
+    <div style="border: 1px solid #ccc; padding: 20px; border-radius: 10px; margin-bottom: 20px;">
+        <div style="display: flex; align-items: center;">  
+            <div style="width: 150px; margin-right: 20px;">  
+                <img src="{image_info}" style="width: 100%; border-radius: 5px;">
+            </div>
+            <div>
+                <h3 style="margin-top: 0;">{key}</h3>
+                <p><strong>Occupation:</strong> {occupation}</p>
+                <p><strong>Education:</strong> {education}</p>
+            </div>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
     col400,col401,col402=st.columns(3)
     with col401:
         st.image(image_info)
@@ -297,7 +331,18 @@ def sharks(season_df):
         sharks_info(season_df,"Aman")
     elif st.session_state.selected_shark == "Peyush":
         sharks_info(season_df,"Peyush")
+    
+    if st.session_state.selected_shark == "Namita":
+        shark_info_card("Namita", "CEO, Emcure Pharmaceuticals", "MBA, Wharton School of the University of Pennsylvania", "Namita.jpg")
         
+    elif st.session_state.selected_shark == "Vineeta":
+        shark_info_card("Vineeta Singh", "CEO, SUGAR Cosmetics", "IIM Ahmedabad", "Vineeta.jpg")
+    elif st.session_state.selected_shark == "Anupam":
+        shark_info_card("Anupam Mittal", "Founder & CEO, Shaadi.com", "MBA, Boston College", "Anupam.jpg")
+    elif st.session_state.selected_shark == "Aman":
+        shark_info_card("Aman Gupta", "Co-Founder and CMO, boAt", "MBA, ISB Hyderabad", "Aman.jpg")
+    elif st.session_state.selected_shark == "Peyush":
+        shark_info_card("Peyush Bansal", "Founder & CEO, Lenskart", "IIM Bangalore", "Peyush.jpg")
         
         
 #pitches details function part 3
