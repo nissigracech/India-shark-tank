127934153b7dc418171bf56fafc1faeeae0e6010
nissigracech <nissigracech@gmail.com>
2025-02-21 21:39:00 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -120,7 +120,7 @@ def count_shark_deal(data, shark_names):
 
 
 def sharks_info(season_df,key):
-    image_info=f"{key}.jpg"
+    image_info=key+".jpg"
     col400,col401,col402=st.columns(3)
     with col401:
         st.image(image_info)
