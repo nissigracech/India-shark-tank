f1ffef13d2130825923a302cedb2b41cc96963f5
nissigracech <nissigracech@gmail.com>
2025-02-22 13:00:56 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -86,12 +86,11 @@ def shark_metric_card(name, occupation, education):
     space=" "
     st.markdown(
         f"""
-        <div class="metric-card">
-            <div class="shark_metric-subtitle"> </div>
+        <div class="metric-card"> 
             <div class="shark_metric-title">{name}</div>
             <div class="shark_metric-title">&nbsp;</div>
             <div class="shark_metric-value">{occupation}</div>
-            <div class="shark_metric-value"> </div>
+            <div class="shark_metric-value">&nbsp;</div>
             <div class="shark_metric-subtitle">{education}</div>
             <div class="shark_metric-value">&nbsp;</div>
         </div>
