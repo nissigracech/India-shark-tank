ea7fd560a5c94741f661d747205c79966dbcef55
nissigracech <nissigracech@gmail.com>
2025-02-23 15:08:09 +0530

    Updated requirements

--- shark_tank.py
+++ shark_tank.py
@@ -506,7 +506,7 @@ st.markdown("---")
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     sub_col1, sub_col2, sub_col3, sub_col4, sub_col5, sub_col6, sub_col7,sub_col8,sub_col9 = st.columns(
-        [4, 10, 3, 10, 3, 10, 3, 10, 2]
+        [1, 10, 2, 10, 2, 10, 2, 10, 1]
     )
 
     with sub_col2:
